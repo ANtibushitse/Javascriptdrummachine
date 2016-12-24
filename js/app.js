5348ea84164384a749c69a961bffaa5fcc45dfef
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-12-24 14:17:44 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ window.addEventListener('keydown', function (e) {
 	if(!audio)return; 	
 	audio.currentTime = 0 ; 
 	audio.play();
-	key.classList.add('playing';)
+	key.classList.add('playing');
 });  
 
  function removeTransition(e){
